fb6838fc3a58104ce656d910ec1daa9571049e7c
Pradeep <mailtobaral@gmail.com>
2019-07-01 09:50:33 -0700

    Added title replacement

--- build.py
+++ build.py
@@ -1,39 +1,108 @@
-# import os
 
-# dir_name = "docs"    #path
-# input_files_index = ["templates/top.html", "content/index.html", "templates/bottom.html"]     #input files
-# file_index_out = "./index.html"     #choose a name for the output file
-# file_output = os.path.join(dir_name, file_index_out)
 
-# fout = open(file_output, 'w')
+pages = [
+        {
+            "filename": "content/index.html", #Input file
+            "output": "docs/index.html",       #output file after templating
+            "title": "Welcome to PB site - Pradeep B.", 
+        },
+        {
+            "filename": "content/portfolio.html",
+            "output": "docs/portfolio.html",
+            "title": "My Portfolio - Pradeep B.",
+        },
+        {
+            "filename": "content/about.html",
+            "output": "docs/about.html",
+            "title": "About the site - Pradeep B.",
+        },
+        {
+            "filename": "content/contact.html",
+            "output": "docs/contact.html",
+            "title": "Contact Us - Pradeep B.",
+        },
+            
+            ] 
+ 
 
-# for tempfile in input_files_index:
-#     inputfile = os.path.join(dir_name, tempfile)
-#     fin = open(inputfile, 'r')
+def generate_html():   
+    for page in pages:
+        file_name = page["filename"]
+        file_output = page["output"]
+        template = open("templates/base.html").read()
+        page_title = page["title"]
+        html_content = open(page["filename"]).read()
+        templated_html = replace_template(html_content, template, page_title)
+        open(file_output, "w+").write(templated_html)
+        print("The file " + file_name + " has been created successfully")
+    return
+
+def replace_template(html_content, template, page_title):
+    templated_html = template.replace("{{content}}", html_content).replace("{{title}}", page_title)        
+    return templated_html
+def main():
     
-#     for line in fin:
-#         fout.write(line)
-        
-# fin.close()    
-# fout.close()
+        generate_html()
+    
+if __name__ == "__main__":
+    main()
 
-index_filenames = ["templates/top.html", "content/index.html", "templates/bottom.html"] #Input file list
-with open('docs/index.html', 'w') as outfile:
-    for fname in index_filenames:
-        with open(fname) as infile:
-            outfile.write(infile.read()) #Output single file
-index_filenames = ["templates/top.html", "content/portfolio.html", "templates/bottom.html"] #Input file list
-with open('docs/portfolio.html', 'w') as outfile:
-    for fname in index_filenames:
-        with open(fname) as infile:
-            outfile.write(infile.read()) #Output single file
-index_filenames = ["templates/top.html", "content/about.html", "templates/bottom.html"] #Input file list
-with open('docs/about.html', 'w') as outfile:
-    for fname in index_filenames:
-        with open(fname) as infile:
-            outfile.write(infile.read()) #Output single file
-index_filenames = ["templates/top.html", "content/contact.html", "templates/bottom.html"] #Input file list
-with open('docs/contact.html', 'w') as outfile:
-    for fname in index_filenames:
-        with open(fname) as infile:
-            outfile.write(infile.read()) #Output single file
+
+#Working code
+# pages = [
+#         {
+#             "filename": "content/index.html",
+#             "output": "docs/index.html",
+#             "title": "Welcome to PB site - Pradeep B.",
+#         },
+#         {
+#             "filename": "content/portfolio.html",
+#             "output": "docs/portfolio.html",
+#             "title": "My Portfolio - Pradeep B.",
+#         },
+#         {
+#             "filename": "content/about.html",
+#             "output": "docs/about.html",
+#             "title": "About the site - Pradeep B.",
+#         },
+#         {
+#             "filename": "content/contact.html",
+#             "output": "docs/contact.html",
+#             "title": "Contact Us - Pradeep B.",
+#         },
+            
+#             ] 
+# def generate_html():   
+#     for page in pages:
+#         file_name = page["filename"]
+#         file_output = page["output"]
+#         template = open("templates/base.html").read()
+#         html_content = open(file_name).read()
+#         finished_html= template.replace("{{content}}", html_content)
+#         open(file_output, "w+").write(finished_html)
+#         print("The file " + file_name + " has been created successfully")
+#     return
+
+
+
+
+    # index_filenames = ["templates/top.html", "content/index.html", "templates/bottom.html"] #Input file list
+    # with open('docs/index.html', 'w') as outfile:
+    #     for fname in index_filenames:
+    #         with open(fname) as infile:
+    #             outfile.write(infile.read()) #Output single file
+    # index_filenames = ["templates/top.html", "content/portfolio.html", "templates/bottom.html"] #Input file list
+    # with open('docs/portfolio.html', 'w') as outfile:
+    #     for fname in index_filenames:
+    #         with open(fname) as infile:
+    #             outfile.write(infile.read()) #Output single file
+    # index_filenames = ["templates/top.html", "content/about.html", "templates/bottom.html"] #Input file list
+    # with open('docs/about.html', 'w') as outfile:
+    #     for fname in index_filenames:
+    #         with open(fname) as infile:
+    #             outfile.write(infile.read()) #Output single file
+    # index_filenames = ["templates/top.html", "content/contact.html", "templates/bottom.html"] #Input file list
+    # with open('docs/contact.html', 'w') as outfile:
+    #     for fname in index_filenames:
+    #         with open(fname) as infile:
+    #             outfile.write(infile.read()) #Output single file
